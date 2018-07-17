c996fa95b16356d43c60ec6315a9bf64506c100f
elias saad <eliassaad@eliass-MacBook-Pro.local>
2018-07-17 06:53:07 +0300

    added UI objects

--- EliasTest/ViewController.swift
+++ EliasTest/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    
     }
 
     override func didReceiveMemoryWarning() {
